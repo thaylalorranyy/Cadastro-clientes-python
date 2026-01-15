2e7b47b54f74ae679c383e5588ffe5d081e3981d
thaylalorranyy <thay.lorrany22@gmail.com>
2026-01-15 17:37:40 -0300

    Implement client registration menu in cadastro.py

---
+++ cadastro.py
@@ -0,0 +1,22 @@
+clientes = []
+def menu(): 
+  print("MENU")
+  print("1- Cadastrar clientes")
+  print("2- Listar clientes")
+  print("0- Sair")
+while true: 
+  menu()
+  Opção = input("Escolha uma opção: ")
+  if opção == "1"
+  nome = input("Digite o nome do cliente")
+  clientes.append(nome)
+  print ("Cliente cadastrado com sucesso")
+elif opção == "2" 
+print ("Clientes cadastrados:")
+for clientes if clientes: 
+  print (clientes) 
+elif opção == "0"
+print ("Saindo do sistema")
+break 
+else:
+print("Opção invalida")
